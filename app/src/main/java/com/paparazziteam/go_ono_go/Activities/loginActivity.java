b6051ffa915430e66095f6345ed3b8cf67c9c30b
Alikhan <73962071@continental.edu.pe>
2021-07-20 14:44:37 -0500

    design UI login

---
+++ app/src/main/java/com/paparazziteam/go_ono_go/Activities/loginActivity.java
@@ -0,0 +1,15 @@
+package com.paparazziteam.go_ono_go.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.paparazziteam.go_ono_go.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/paparazziteam/go_ono_go/MainActivity.java
@@ -0,0 +1,14 @@
+package com.paparazziteam.go_ono_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
